ddf99f56e8550cac628b4e80d2c0e78cba6dd3b2
nikdh004 <nikdh004@gmail.com>
2024-09-26 18:05:27 +0530

    Create interactivity.py

---
+++ interactivity.py
@@ -0,0 +1,15 @@
+class Tooltip:
+    def __init__(self, chart):
+        self.chart = chart
+
+    def add_tooltip(self, text):
+        # Logic to add a tooltip to the chart
+        pass
+
+class Animation:
+    def __init__(self, chart):
+        self.chart = chart
+
+    def animate(self, duration=1000):
+        # Logic to add animations to the chart
+        pass
