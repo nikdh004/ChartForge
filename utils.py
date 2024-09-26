1c7767612316d51caf07a45dbc0c35b1d92c277e
nikdh004 <nikdh004@gmail.com>
2024-09-26 18:05:02 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def scale_data(data):
+    # Logic to scale the data if necessary
+    pass
+
+def parse_data(data):
+    # Helper to parse data from Pandas or other formats
+    return pd.DataFrame(data)
